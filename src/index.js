025a5d05f63fd111e2cd96a84f3a7036e4fe54c8
Maurice Melchers <maurice.melchers@gmail.com>
2018-10-03 14:36:39 +0800

    changed project name, added basic set up cmds

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('oh hai world')
+import express from 'express'; 
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Oh hi World')
+})
+app.listen(4000, () => {
+  console.log('Listening');
+});
